aa58b365d11324084aa1b488a2bb231ea1539ba2
amankeldi.kurban <dimitesting31@gmail.com>
2024-04-09 22:24:25 +0300

    clearing default react app and setting up new project

---
+++ src/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAKSQqGwfjWkuAUrV26Q_qQ0SNYPIr3aTM",
+  authDomain: "house-market-app-a8aa9.firebaseapp.com",
+  projectId: "house-market-app-a8aa9",
+  storageBucket: "house-market-app-a8aa9.appspot.com",
+  messagingSenderId: "523885974962",
+  appId: "1:523885974962:web:4b027d30c98da92b4540ab"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
+
