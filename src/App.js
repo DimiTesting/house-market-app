55b3e6eee6f199a61223947551346ecdfb8debc7
amankeldi.kurban <dimitesting31@gmail.com>
2024-04-13 18:48:29 +0300

    Adding privateRoute component, spinner and custom hook

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Offers from "./pages/Offers"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Profile from "./pages/Profile"
+import PrivateRoute from "./components/PrivateRoute";
 
 
 function App() {
@@ -17,8 +18,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore/>}/>
           <Route path="/offers" element={<Offers/>}/>
+          <Route path='/profile' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
           <Route path="/sign-in" element={<SignIn/>}/>
-          <Route path="/profile" element={<Profile/>}/>
           <Route path="/sign-up" element={<SignUp/>}/>
           <Route path="/forgotpassword" element={<ForgotPassword/>}/>
         </Routes>
