0436511a5516df3f13e6ffb5401de7f74db593a6
amankeldi.kurban <dimitesting31@gmail.com>
2024-04-14 13:54:15 +0300

    setting up Category page

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,7 +1,25 @@
+import {Link} from "react-router-dom"
+import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg"
+import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg"
+
 function Explore() {
     return (
-        <div>
-            <h1> Explore </h1>
+        <div className="explore">
+            <p className="pageHeader"> Explore </p>
+
+            <main>
+                <p className="exploreCategoryHeading">Categories</p>
+                <div className="exploreCategories">
+                    <Link to="/category/rent">
+                        <img className="exploreCategoryImg" src={rentCategoryImage} alt="rent"/>
+                        <p className="exploreCategoryName"> Places for Rent </p>
+                    </Link>
+                    <Link to="/category/sell">
+                        <img className="exploreCategoryImg" src={sellCategoryImage} alt="rent"/>
+                        <p className="exploreCategoryName"> Place for Sell </p>
+                    </Link>
+                </div>
+            </main>
         </div>
     )
 }
