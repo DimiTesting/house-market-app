82ced8a55c11e538f443b4bac8978c95b2609b80
amankeldi.kurban <dimitesting31@gmail.com>
2024-05-13 17:06:59 +0300

    fixing the app

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,17 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import dotenv from "dotenv"
-dotenv.config()
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
+  apiKey: "AIzaSyAKSQqGwfjWkuAUrV26Q_qQ0SNYPIr3aTM",
   authDomain: "house-market-app-a8aa9.firebaseapp.com",
   projectId: "house-market-app-a8aa9",
   storageBucket: "house-market-app-a8aa9.appspot.com",
   messagingSenderId: "523885974962",
-  appId: process.env.APP_ID
+  appId: "1:523885974962:web:4b027d30c98da92b4540ab", 
 };
 
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 export const db = getFirestore()
 
